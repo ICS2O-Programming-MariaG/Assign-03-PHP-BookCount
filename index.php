6ab3696acf0bfe8fc76aa5817601daa0aba2d63f
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-04-21 01:07:52 +0000

    Added introductory text to PHP web page

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en-ca">
+  <head>
+    <!-- Metadata -->
+    <meta charset="utf-8">
+    <meta name="description" content="Maria's Fahrenheit to Celsius Webpage">
+    <meta name="keywords" content="immaculata, ics2o">
+    <meta name="author" content="MariaG">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Code for the favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
+    <link rel="manifest" href="./favicon/site.webmanifest">
+    <!-- Link to CSS file -->
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- Title -->
+    <title>Book Count - Calculating Page Count for Books - Maria G</title>
+  </head>
+  <body>
+    <?php echo "<h1>Book Count</h1>"; ?>
+    <?php echo "<p>Welcome to the second page of Book Count. This web page will calculate the approximate number of pages in a book given a known word count.</p>"; ?>
+  </body>
+</html>
