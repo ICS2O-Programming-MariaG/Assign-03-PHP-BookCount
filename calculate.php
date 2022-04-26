98671be50ed2cac48494eb8a1ed53b993757f991
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-04-26 01:33:11 +0000

    Updated calculations using comparison operator ==

--- calculate.php
+++ calculate.php
@@ -10,39 +10,39 @@
   //if, then, else statements learned at https://www.w3schools.com/php/php_if_else.asp
 
   //First if/then statement: if page size is 5 x 8 (option value is 1)
-  if ($pageSize < 2) {
+  if ($pageSize == 1) {
     //Font size if/then statement: if fontSize is 10
-    if ($fontSize < 11) {
+    if ($fontSize == 10) {
       $pageCount = number_format($wordCount / 400, 2);
       echo "The book has approximately $pageCount pages filled with text.";
     }
-    //Second if/then statement: if fontSize is 12
-    else if ($fontSize > 11) {
-      $pageCount = number_format($wordCount / 300, 2);
+    //Second if/then statement: if fontSize is 11
+    else if ($fontSize == 11) {
+      $pageCount = number_format($wordCount / 350, 2);
       echo "The book has approximately $pageCount pages filled with text.";
     }
-    //Third if/then statement: if fontSize is 11 (both other situations are false)
+    //Third if/then statement: if fontSize is 12 (both other situations are false)
     else {
-      $pageCount = number_format($wordCount / 350, 2);
+      $pageCount = number_format($wordCount / 300, 2);
       echo "The book has approximately $pageCount pages filled with text.";
     }
   }
 
   //Second deciding option: if page size is 6 x 9 (option value is 2)
-  else if ($pageSize > 1) {
+  else if ($pageSize == 2) {
     //Font size 10
-    if ($fontSize < 11) {
+    if ($fontSize == 10) {
       $pageCount = number_format($wordCount / 600, 2);
       echo "The book has approximately $pageCount pages filled with text.";
     }
-    //fontSize is 12
-    else if ($fontSize > 11) {
-      $pageCount = number_format($wordCount / 425, 2);
+    //fontSize is 11
+    else if ($fontSize == 11) {
+      $pageCount = number_format($wordCount / 500, 2);
       echo "The book has approximately $pageCount pages filled with text.";
     }
-    //fontSize is 11 (both other situations are false)
+    //fontSize is 12 (both other situations are false)
     else {
-      $pageCount = number_format($wordCount / 500, 2);
+      $pageCount = number_format($wordCount / 425, 2);
       echo "The book has approximately $pageCount pages filled with text.";
     }
   }
